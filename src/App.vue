98939b760d0e44b59c4522c1e8be82ef6320ffa9
Henrico <hicolazu22@hotmail.com>
2020-06-12 22:45:31 -0300

    final commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="app">
+        <h1>Calculadora</h1>
+        <Calculator /> 
+    </div>
+</template>
+
+<script>
+import Calculator from './main/Calculator';
+
+export default {
+    components: { Calculator }
+}
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300&display=swap');
+
+    * {
+        font-family: 'RobotoMono', monospace;
+    }
+
+    body {
+        margin: 0;
+    }
+
+    #app {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+
+        color: #ffffff;
+        background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+    }
+</style>
